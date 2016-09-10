ac971794dcf36d80351304ab87780a30b8abac8c
zhuangqian <smallchill@163.com>
2016-09-10 14:35:12 +0800

    jsonkit 增加JSONObejct返回

--- src/main/java/com/smallchill/core/toolbox/kit/JsonKit.java
+++ src/main/java/com/smallchill/core/toolbox/kit/JsonKit.java
@@ -1,35 +1,21 @@
-/**
- * Copyright (c) 2011-2016, James Zhan 詹波 (jfinal@126.com).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.smallchill.core.toolbox.kit;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
-/**
- * JsonKit.
- */
+
 public class JsonKit {
 	
 	public static String toJson(Object object) {
-		return JSON.toJSONStringWithDateFormat(object, "yyyy-MM-dd HH:mm:ss", SerializerFeature.WriteDateUseDateFormat);
+		return JSONObject.toJSONStringWithDateFormat(object, "yyyy-MM-dd HH:mm:ss", SerializerFeature.WriteDateUseDateFormat);
 	}
 	
-	public static <T> T parse(String jsonString, Class<T> type) {
-		return JSON.parseObject(jsonString, type);
+	public static JSONObject parse(String text) {
+		return JSONObject.parseObject(text);
+	}
+	
+	public static <T> T parse(String text, Class<T> clazz) {
+		return JSONObject.parseObject(text, clazz);
 	}
 	
 }
