2bcf385e31fc4cb0a4d4286566bd5079f9440c30
smallchill <smallchill@163.com>
2017-08-15 21:31:33 +0800

    httpkit增加json格式参数

--- src/main/java/org/springblade/core/toolbox/kit/HttpKit.java
+++ src/main/java/org/springblade/core/toolbox/kit/HttpKit.java
@@ -1,29 +1,33 @@
 package org.springblade.core.toolbox.kit;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import javax.servlet.http.HttpServletRequest;
-
+import org.springblade.core.toolbox.ajax.AjaxResult;
 import org.springblade.core.toolbox.support.WafRequestWrapper;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class HttpKit {
 	private static PoolingHttpClientConnectionManager cm;
@@ -99,6 +103,35 @@ public class HttpKit {
 
 		return getResult(httpPost);
 	}
+
+    public static String post(String url, String json) {
+        String returnValue = JsonKit.toJson(new AjaxResult().addFail("接口调用失败"));
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        try {
+            //第一步：创建HttpClient对象
+            httpClient = HttpClients.createDefault();
+            //第二步：创建httpPost对象
+            HttpPost httpPost = new HttpPost(url);
+            //第三步：给httpPost设置JSON格式的参数
+            StringEntity requestEntity = new StringEntity(json, "utf-8");
+            requestEntity.setContentEncoding("UTF-8");
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setEntity(requestEntity);
+            //第四步：发送HttpPost请求，获取返回值
+            returnValue = httpClient.execute(httpPost, responseHandler); //调接口获取返回值时，必须用此方法
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //第五步：处理返回值
+        return returnValue;
+    }
 
 	private static ArrayList<NameValuePair> covertParams2NVPS(Map<String, Object> params) {
 		ArrayList<NameValuePair> pairs = new ArrayList<NameValuePair>();
