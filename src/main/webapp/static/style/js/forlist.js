fada028c58da09ba23534f1a2cc8d786a77fbd0f
smallchill <smallchill@163.com>
2017-10-09 16:15:56 +0800

    jqgrid翻页记录之前选中行

--- src/main/webapp/static/style/js/forlist.js
+++ src/main/webapp/static/style/js/forlist.js
@@ -1,6 +1,7 @@
 /**
-* 表格默认配置
-*/
+ * 表格默认配置
+ */
+var item_selected = [];
 jQuery.extend(jQuery.jgrid.defaults, {
     datatype: "json",
     autowidth: true,
@@ -11,7 +12,7 @@ jQuery.extend(jQuery.jgrid.defaults, {
     multiboxonly: true,
     jsonReader: { id: "id" }, //将后台返回的id设置为表的主键
     viewrecords: true,
-    rowNum: 10,
+    rowNum: 20,
     rowList: [10, 20, 30, 50, 100],
     rownumbers: true,
     loadComplete: function () {
@@ -19,9 +20,60 @@ jQuery.extend(jQuery.jgrid.defaults, {
         setTimeout(function () {
             updatePagerIcons(table);
             //自适应表格高度 -- 表格初始化后调用
-            autoResize(grid_selector, 215);
+            autoResize(grid_selector, 225);
             $jqGrid = $(grid_selector);
         }, 0);
+
+        //grid加载完毕，从数组中选中对应的记录
+        if(item_selected.length>0){
+            for (var i = 0; i < item_selected.length; i++) {
+                $jqGrid.jqGrid('setSelection',item_selected[i]);
+            }
+        }
+    }
+    ,onSelectAll:function(aRowids,status) {
+        //全选事件触发的事件
+        if(status){
+            item_selected = _.union(item_selected, aRowids);
+        }else{
+            item_selected = _.difference(item_selected, aRowids);
+        }
+    },
+    beforeSelectRow:function(rowid, e) {
+
+        //CheckBox区域的td点击后不触发选中事件
+
+        var flag = true
+
+        if (e.target.firstChild != undefined) {
+            //选择非CheckBox区域，禁止默认点击事件，调用该行对应的CheckBox的点击事件
+            if(e.target.firstChild.className == undefined) {
+                $("#jqg_grid-table_" + rowid).click()
+                flag = false
+            }
+            //选择CheckBox区域的td区域，禁止默认点击事件，调用该行对应的CheckBox的点击事件
+            if(e.target.firstChild.className == "cbox") {
+                $("#" + e.target.firstChild.id).click()
+                flag = false
+            }
+        }
+        if (e.target.type == "checkbox"){
+            //CheckBox的点击事件
+            flag = true
+        }
+
+        if (flag) {
+            //点击则从数组增加，否则从数组移除
+            var index = _.indexOf(item_selected, rowid);
+            if(index==-1){
+                item_selected.push(rowid);
+            }
+            else{
+                item_selected = _.pull(item_selected, rowid);
+            }
+        }
+
+        return flag
     }
 });
 
@@ -30,12 +82,12 @@ jQuery.extend(jQuery.jgrid.defaults, {
 //replace icons with FontAwesome icons like above
 function updatePagerIcons(table) {
     var replacement =
-	{
-	    'ui-icon-seek-first': 'ace-icon fa fa-angle-double-left bigger-140',
-	    'ui-icon-seek-prev': 'ace-icon fa fa-angle-left bigger-140',
-	    'ui-icon-seek-next': 'ace-icon fa fa-angle-right bigger-140',
-	    'ui-icon-seek-end': 'ace-icon fa fa-angle-double-right bigger-140'
-	};
+        {
+            'ui-icon-seek-first': 'ace-icon fa fa-angle-double-left bigger-140',
+            'ui-icon-seek-prev': 'ace-icon fa fa-angle-left bigger-140',
+            'ui-icon-seek-next': 'ace-icon fa fa-angle-right bigger-140',
+            'ui-icon-seek-end': 'ace-icon fa fa-angle-double-right bigger-140'
+        };
     $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function () {
         var icon = $(this);
         var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
@@ -46,11 +98,11 @@ function updatePagerIcons(table) {
 
 
 /**
-* jqgrid自适应屏幕
-* @author taoyunjie
-* @param id,fixHeight
-* @returns 
-*/
+ * jqgrid自适应屏幕
+ * @author taoyunjie
+ * @param id,fixHeight
+ * @returns
+ */
 function autoResize(idObj, fixHeight) {
     //初始化时
     $(idObj).setGridHeight($(window).height() - fixHeight);
@@ -74,13 +126,15 @@ function autoResize(idObj, fixHeight) {
 
 //获取表格单条记录xl（多条时 选取最后选中的）
 function getGridXl() {
-    return $jqGrid.jqGrid('getGridParam', 'selrow');
+    return item_selected[0];
+    //return $jqGrid.jqGrid('getGridParam', 'selrow');
 }
 
 
 //获取多行记录的xl值
 function getGridXls() {
-    return $jqGrid.jqGrid("getGridParam", "selarrrow");
+    return item_selected;
+    //return $jqGrid.jqGrid("getGridParam", "selarrrow");
 }
 
 function getRowData() {
