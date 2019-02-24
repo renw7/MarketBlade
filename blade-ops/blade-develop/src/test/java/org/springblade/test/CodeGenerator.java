60fd7402f7ae1b01e3accdf874e1e0bf0eea2a20
smallchill <smallchill@163.com>
2019-02-24 12:11:36 +0800

    :zap: 增加saber代码生成功能

--- blade-ops/blade-develop/src/test/java/org/springblade/test/CodeGenerator.java
+++ blade-ops/blade-develop/src/test/java/org/springblade/test/CodeGenerator.java
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springblade.develop.support;
+package org.springblade.test;
 
+
+import org.springblade.develop.support.BladeGenerator;
 
 /**
  * 代码生成器
