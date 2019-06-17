33ee78c4ca7b174862b78d2559d2b16b32e26468
smallchill <smallchill@163.com>
2019-06-17 08:30:18 +0800

    :tada: 2.3.2.RELEASE

--- src/main/java/org/springblade/modules/resource/OssEndpoint.java
+++ src/main/java/org/springblade/modules/resource/OssEndpoint.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springblade.modules.resources;
+package org.springblade.modules.resource;
 
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
